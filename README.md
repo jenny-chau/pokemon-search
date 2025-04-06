d2ab46c7825fdc8b7357447503b58eee0e9fbbde
jenny-chau <geeyun.jenny.chau@gmail.com>
2025-04-06 12:58:19 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Pokemon Search
+
+Introduction:
+- A simple webpage to search for basic information about any pokemon using the PokeAPI. 
+
+Features:
+- Webpage will display the image, pokemon name, and pokemon type. 
+- If the pokemon is not found, an error message is display instead. 
+- The search button can be clicked or can be activated by pressing the "Enter" key.
+- The reset button resets the input field.
+
+Code:
+- Webpage incorporates html, css, and javascript (async function to fetch API data).
---
+++ search.js
@@ -0,0 +1,57 @@
+// async function to fetch API data
+async function fetchData(callBack) {
+    const userInput = document.getElementById("inputValue").value;
+    const apiLink = "https://pokeapi.co/api/v2/pokemon/" + userInput;
+
+    try {
+        const pokemonData = await fetch(apiLink);
+        const useableData = await pokemonData.json();
+        callBack(null, useableData);
+    } catch(error) {
+        callBack(error, null);
+    }
+}
+
+// callBack function to display the data/error message on the page
+const displayData = function (error, data) {
+    const displayElement = document.getElementById("display-pokemon-info");
+    
+    if(error) {
+        displayElement.innerHTML = `
+            <p>Pokemon not found. Pleace check spelling/symbols.</p>
+            <br>
+            Error fetching data: ${error}
+        `;
+    } else {
+        const name = data.name;
+        const type = data['types']['0']['type']['name'];
+
+        displayElement.innerHTML = `
+            <img src="${data.sprites.front_default}" placeholder="${data.name} image">
+            <p><strong>Name:</strong> ${name.charAt(0).toUpperCase() + name.slice(1)}</p>
+            <p><strong>Type:</strong> ${type.charAt(0).toUpperCase() + type.slice(1)}</p>
+        `; //Capitalizes first letter of name and type
+        
+        resetPokemonValue();
+    }
+    displayElement.style.visibility = 'visible';
+};
+
+// Called in index.html when search button is clicked
+const searchPokemon = function() {
+    fetchData(displayData);
+};
+
+// Reset button function
+const resetPokemonValue = function() {
+    const inputElement = document.getElementById("inputValue");
+    inputElement.value = "";
+};
+
+// Allows input to be submitted via pressing "Enter" key on keyboard
+let input = document.getElementById("inputValue");
+input.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        document.getElementById("submitBtn").click();
+    }
+});
